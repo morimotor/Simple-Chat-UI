488ee5e9126641a8f8e0d0ad08e2380d8d79e2be
morimotor <emuryu@gmail.com>
2015-10-21 15:43:34 +0900

    initial commit

---
+++ app/src/main/java/jp/morimotor/chatui/MainActivity.java
@@ -0,0 +1,35 @@
+package jp.morimotor.chatui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private Button mButton;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView)findViewById(R.id.listView);
+        mEditText = (EditText)findViewById(R.id.editText);
+        mButton = (Button)findViewById(R.id.button);
+
+
+
+    }
+
+
+
+
+
+
+}
